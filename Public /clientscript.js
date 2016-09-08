289dfaacfd823ce48b3c5defc2b8d52b48d3f09d
John Arkema <john.arkema@gmail.com>
2016-09-08 14:22:42 -0500

    more client side

--- Public /clientscript.js
+++ Public /clientscript.js
@@ -2,30 +2,40 @@ console.log('The client script is sourced, Dave');
 
 $(document).ready(function(){
 console.log('Document ready, Dave');
-var guessOne = 0;
-var guessTwo = 0;
-var guessThree = 0;
-var guessFour = 0;
+// var guessOne = 0;
+// var guessTwo = 0;
+// var guessThree = 0;
+// var guessFour = 0;
+
+var guessData = [ guessOne, guessTwo, guessThree, guessFour ];
+
 
 //gameStart
 //guess
 
 
-$('#submit').on('click', function(){
-  console.log('Submitting, Dave');
+$('#gameStart').on('click', function(){
+  console.log('Game Starting, Dave');
   $.ajax({
     type:'POST',
-    url:"/submit",
-    data: submitData,
+    url:"/gameStart",
+    data: gameStartData,
     success: function(data){
-      console.log('Data Sumbit' + data);
+      console.log('gameStart - ' + data);
     }//end Success
   });//end ajax /submit
 
+  $('#guess').on('click', function(){
+    console.log('Sending Guess to server, Dave');
+    $.ajax({
+      type: 'POST',
+      url: '/guess',
+      data: guessData,
+      success: function(){
+        console.log('guesses - ' + data );
+      }//end Guess
+    });//end guess ajax
+  });//end guess click
+
 });//end submit click function
-
-
-
-
-
 });//end document ready
