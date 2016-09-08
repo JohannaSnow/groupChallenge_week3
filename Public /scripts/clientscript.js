e8201b8677189b56d4c00b9691ede4c55e33a5d5
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-08 14:54:24 -0500

    file restructure

--- Public /scripts/clientscript.js
+++ Public /scripts/clientscript.js
@@ -9,33 +9,58 @@ console.log('Document ready, Dave');
 
 var guessData = [ guessOne, guessTwo, guessThree, guessFour ];
 
+var gameStartData = //maxNumber
 
 //gameStart
 //guess
 
 
-$('#gameStart').on('click', function(){
+$('#startButton').on('click', function(){
   console.log('Game Starting, Dave');
   $.ajax({
     type:'POST',
     url:"/gameStart",
     data: gameStartData,
     success: function(data){
-      console.log('gameStart - ' + data);
+      console.log('gameStart call successful');
+      $('.container').fadeOut(400, function(){
+        $('#playScreen').fadeIn();
+      });
+      //----do stuff to the DOM, change to game page
     }//end Success
   });//end ajax /submit
+});//end gameStart click function
 
-  $('#guess').on('click', function(){
-    console.log('Sending Guess to server, Dave');
-    $.ajax({
-      type: 'POST',
-      url: '/guess',
-      data: guessData,
-      success: function(data){
-        console.log('guesses - ' + data );
-      }//end Guess
-    });//end guess ajax
-  });//end guess click
+$('#playSubmit').on('click', function(){
+  console.log('Sending Guess to server, Dave');
+  $.ajax({
+    type: 'POST',
+    url: '/guess',
+    data: guessData,
+    success: function(data){
+      console.log('guesses - ' + data );
+      if (/*someone wins*/) {
+        $('.container').fadeOut(400, function(){
+          $('#resultScreen').fadeIn();
+        });
+      }
+      //----do stuff to the DOM with data
+    }//end Guess
+  });//end guess ajax
+});//end guess click
 
-});//end submit click function
+$('#playQuit').on('click', function(){
+  //do stuff to DOM, change to result page
+  $('.container').fadeOut(400, function(){
+    $('#resultScreen').fadeIn();
+  });
+})
+
+$('#resultRestart').on('click', function(){
+  //do stuff to DOM, change to start page
+  $('.container').fadeOut(400, function(){
+    $('#startScreen').fadeIn();
+  });
+})
+
 });//end document ready
